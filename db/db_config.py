857ff64b7fb1ed77b6debb2e826e7f0eba9ee340
Larissa Botelho <larissabotelho2021@gmail.com>
2025-02-26 17:55:42 -0300

    remove pipfile and add requirements.txt. Fix frontend default values and scripts. update readme. delete db_config to use .env values

--- db/db_config.py
+++
@@ -1,2 +0,0 @@
-config = { 'MONGO_DB': 'letterboxd', 'CONNECTION_URL': 'mongodb://localhost:27017/letterboxd', 'MONGO_USERNAME': 'larissa', 'MONGO_PASSWORD': 'lari123', 'MONGO_CLUSTER_ID': '6r3bs'}
-tmdb_key = 'b6aa9e3a5640442db50f18523bfcb52a'
